83882960ecbb5fc725c56e9583a49a8f3b9e2b1f
Michael Causey <michaelhcausey@gmail.com>
2020-05-19 00:13:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
-  new_array = array.map { |num| num * num}
+  new_array = array.map do |num|
+    num * num
+  end
   new_array
 end
 
